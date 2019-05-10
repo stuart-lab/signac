1bf386ab1edf94d4185a99c3266b73e3e3c9d41a
timoast <4591688+timoast@users.noreply.github.com>
2019-05-10 13:58:26 -0400

    first working version of CoveragePlot

--- R/visualization.R
+++ R/visualization.R
@@ -2,30 +2,67 @@
 #' Plot coverage within given region for groups of cells
 #'
 #' @param object A Seurat object
-#' @param coords A set of genomic coordinates to show
+#' @param region A set of genomic coordinates to show
 #' @param annotation A set of genomic feature annotations. Must be the name of a set of features
 #' stored using the \code{SetAnnotations} function
 #' @param assay Which assay to plot
 #' @param fragment.path Path to an index fragment file. If NULL, will look for a path stored for the
 #' requested assay using the \code{SetFragments} function
 #' @param cells Which cells to plot. Default all cells
+#' @param window Smoothing window size
+#' @param downsample Fraction of positions to retain in the plot. Default is 0.1 (retain 10 percent, ie every 10th position)
 #' @param group.by Name of one or more metadata columns to group (color) the cells by. Default is the current cell identities
 #' @param cols Vector of colors, where each color corresponds to an identity class. By default, use the ggplot2 colors.
 #'
+#' @importFrom ggplot2 geom_bar facet_wrap xlab ylab theme_classic aes ylim theme
 #' @export
 #'
 CoveragePlot <- function(
   object,
-  coords,
+  region,
   annotation = NULL,
   assay = NULL,
   fragment.path = NULL,
   group.by = NULL,
+  window = 100,
+  downsample = 0.1,
   cells = NULL,
   cols = NULL
 ) {
-  # TODO
-  return()
+  reads <- GetReadsInRegion(
+    object = object,
+    assay = assay,
+    region = region,
+    cells = cells,
+    group.by = group.by,
+    fragment.path = fragment.path,
+    verbose = FALSE
+  )
+  coverages <- CalculateCoverages(
+    reads = reads,
+    window = window
+  )
+  if (downsample > 1) {
+    warning('Requested downsampling <0%, retaining all positions')
+    downsample <- 1
+  }
+  chromosome <- unlist(strsplit(region, ':'))[[1]]
+  stepsize <- 1 / downsample
+  total_range <- coverages[nrow(coverages), 'position'] - coverages[1, 'position']
+  steps <- ceiling(total_range / stepsize)
+  retain_positions <- seq(coverages[1, 'position'], coverages[nrow(coverages), 'position'], by = stepsize)
+  downsampled_coverage <- coverages[coverages$position %in% retain_positions, ]
+  ymax <- round(max(downsampled_coverage$coverage, na.rm = TRUE), digits = 2)
+  p <- ggplot(downsampled_coverage, aes(position, coverage, fill = group)) +
+    geom_bar(stat = 'identity') +
+    facet_wrap(~group, strip.position = 'right', ncol = 1) +
+    xlab(paste0(chromosome, ' position (bp)')) +
+    ylab(paste0('Normalized coverage (range 0 - ', as.character(ymax), ')')) +
+    ylim(c(0, ymax)) +
+    theme_classic() +
+    theme(axis.text.y = element_blank())
+  return(p)
+  # TODO add optional gene annotation track
 }
 
 #' Plot coverage pileup centered on a given genomic feature
@@ -51,6 +88,11 @@ PileupPlot <- function(
   cols = NULL
 ) {
   # TODO
+  # need to go from set of annotations to list of positions (single base)
+  # separate into + and - strand
+  # get upstream / downstream for each
+  # invert the - strand and add to the + strand reads
+  # plot histogram with cell groupings (as for PeriodPlot)
   return()
 }
 
