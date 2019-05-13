4261eaaa947df0cf3c228722e2a03c6a935d6dc3
timoast <4591688+timoast@users.noreply.github.com>
2019-05-13 15:44:26 -0400

    add atac_small dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' A small example scATAC-seq dataset
+#'
+#' A subsetted version of 10x Genomics 10k PBMC scATAC-seq dataset
+#'
+#' @format A Seurat object with the following assays
+#'
+#' \describe{
+#'   \item{peaks}{A peak x cell dataset}
+#'   \item{bins}{A 5 kb genome bin x cell dataset}
+#' }
+#'
+#' @source https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k
+"atac_small"
