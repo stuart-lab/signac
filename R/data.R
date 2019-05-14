b8281d2c7d1964d495a273ea044d3e45cfc15b7b
timoast <4591688+timoast@users.noreply.github.com>
2019-05-14 13:41:55 -0400

    add blacklist regions

--- R/data.R
+++ R/data.R
@@ -11,3 +11,27 @@
 #'
 #' @source https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k
 "atac_small"
+
+#' Genomic blacklist regions for Human hg19/GRCh38
+#'
+#' @format A GRanges object
+#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+"blacklist_hg38"
+
+#' Genomic blacklist regions for Mouse mm10
+#'
+#' @format A GRanges object
+#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+"blacklist_mm10"
+
+#' Genomic blacklist regions for Drosophila dm3
+#'
+#' @format A GRanges object
+#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+"blacklist_dm3"
+
+#' Genomic blacklist regions for C. elegans ce10s
+#'
+#' @format A GRanges object
+#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+"blacklist_ce10"
