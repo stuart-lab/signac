91c8cef48c0ee36d92e1062d43b0f73a95871658
timoast <4591688+timoast@users.noreply.github.com>
2019-05-16 12:52:46 -0400

    Add permutation testing to motif enrichments

--- R/enrichments.R
+++ R/enrichments.R
@@ -1,28 +1,99 @@
 #' FindMotifs
 #'
-#' Find motifs enriched in a given set of peaks
+#' Find motifs enriched in a given set of peaks.
+#'
+#' Performs matrix multiplication between a motix x feature and feature x cell matrix to produce motif
+#' counts per cell. This is repeated using a set of background features to calculate the relative enrichment
+#' of each motif in tested features. A permutation test can also be performed by setting the \code{permute}
+#' argument, and the enrichment testing will be repeated \emph{n} times using features sampled at random from
+#' the background set. A p-value is then computed for each motif, which is equal to the number of times
+#' a permuted enrichment score greater or equal to the tested enrichment score was observed.
 #'
 #' @param object A Seurat object
-#' @param cells A vector of cells to test
+#' @param features A vector of features to test
+#' @param cells A vector of cells to include in enrichment tests
 #' @param assay Which assay to use. Default is the active assay
+#' @param permute Number of times to permute the set of features. If NULL, do not perform any permutation
+#' @param background Set of background features to use when calculating relative enrichment. If NULL
+#' use all features present in the object
+#' @param verbose Display messages
 #'
 #' @importFrom Matrix rowSums
+#' @importFrom future plan
+#' @importFrom future.apply future_sapply
+#'
 #' @return Returns a data.frame
 #' @export
 FindMotifs <- function(
   object,
   cells = NULL,
   features = NULL,
-  assay = NULL
+  assay = NULL,
+  permute = NULL,
+  background = NULL,
+  verbose = TRUE
 ) {
   assay <- assay %||% DefaultAssay(object = object)
   cells <- cells %||% colnames(x = object)
   features <- features %||% rownames(x = object)
-  feature.matrix <- GetAssayData(object = object, assay = assay, slot = 'data')[features, cells]
-  motif.matrix <- GetMotifData(object = object, assay = assay, slot = 'data')[features, ]
-  enrichment <- t(motif.matrix) %*% feature.matrix
-  # TODO Check the Cusanovich papers to see how they did this.
-  top.motifs <- sort(x = rowSums(x = enrichment), decreasing = TRUE)
-  results <- data.frame(motif = names(x = top.motifs), score = top.motifs, row.names = names(top.motifs))
-  return(results)
+  background <- background %||% rownames(x = object)
+  if (verbose) {
+    message('Testing motif enrichment in ', length(features), ' regions')
+  }
+  data.use <- GetAssayData(object = object, assay = assay, slot = 'counts')[, cells]
+  motif.all <- GetMotifData(object = object, assay = assay, slot = 'data')
+  top.motifs <- TestEnrichment(
+    motif.matrix = motif.all,
+    feature.matrix = data.use[features, ]
+  )
+  background.scores <- TestEnrichment(
+    motif.matrix = motif.all,
+    feature.matrix = data.use[background, ]
+  )
+  enrichment <- sort(x = top.motifs / background.scores, decreasing = TRUE)
+  results <- data.frame(
+    motif = names(x = enrichment),
+    score = enrichment,
+    row.names = names(enrichment)
+  )
+  if (is.null(x = permute)) {
+    return(results)
+  } else {
+    message("Permuting feature sets ", permute, " times")
+    n.features <- length(features)
+    if (PlanThreads() > 1) {
+      mysapply <- future_sapply
+    } else {
+      mysapply <- ifelse(test = verbose, yes = pbsapply, no = sapply)
+    }
+    permuted.scores <- mysapply(
+      X = 1:permute,
+      FUN = function(x) {
+        rand.sample <- sample(x = background, size = n.features, replace = FALSE)
+        permuted.enrichment <- TestEnrichment(
+          motif.matrix = motif.all,
+          feature.matrix = data.use[rand.sample, ]
+        )
+        return(permuted.enrichment / background.scores)
+      })
+    test.results <- permuted.scores >= enrichment
+    p.vals <- rowSums(test.results) / permute
+    results$pvalue <- p.vals
+    return(results)
+  }
+}
+
+#' TestEnrichment
+#'
+#' Compute motif counts per cell for a given set of features
+#'
+#' @param motif.matrix A feature x motif matrix
+#' @param feature.matrix A feature x cell matrix
+#' @return Returns a vector of motif counts
+TestEnrichment <- function(motif.matrix, feature.matrix) {
+  features = rownames(x = feature.matrix)
+  motif.matrix <- motif.matrix[features, ]
+  enrichment <- crossprod(x = motif.matrix, y = feature.matrix)
+  motif.counts <- rowSums(x = enrichment)
+  return(motif.counts)
 }
