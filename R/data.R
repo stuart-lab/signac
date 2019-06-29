b940048899b28b6ca9d5bef9642a2e0a4b87eef8
timoast <4591688+timoast@users.noreply.github.com>
2019-06-29 15:45:34 -0400

    Update blacklist regions to v2, update citation

--- R/data.R
+++ R/data.R
@@ -9,35 +9,54 @@
 #'   \item{bins}{A 5 kb genome bin x cell dataset}
 #' }
 #'
-#' @source https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k
+#' @source \url{https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k}
 "atac_small"
 
 #' Genomic blacklist regions for Human GRCh38
 #'
 #' @format A GRanges object
-#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
 "blacklist_hg38"
 
 #' Genomic blacklist regions for Human hg19
 #'
 #' @format A GRanges object
-#' @source https://www.encodeproject.org/annotations/ENCSR636HFF/
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
 "blacklist_hg19"
 
 #' Genomic blacklist regions for Mouse mm10
 #'
 #' @format A GRanges object
-#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
 "blacklist_mm10"
 
 #' Genomic blacklist regions for Drosophila dm3
 #'
 #' @format A GRanges object
-#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
 "blacklist_dm3"
+
+#' Genomic blacklist regions for Drosophila dm6
+#'
+#' @format A GRanges object
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
+"blacklist_dm6"
 
 #' Genomic blacklist regions for C. elegans ce10
 #'
 #' @format A GRanges object
-#' @source https://sites.google.com/site/anshulkundaje/projects/blacklists
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
 "blacklist_ce10"
+
+#' Genomic blacklist regions for C. elegans ce11
+#'
+#' @format A GRanges object
+#' @source \url{https://github.com/Boyle-Lab/Blacklist}
+#' @source \url{https://doi.org/10.1038/s41598-019-45839-z}
+"blacklist_ce11"
