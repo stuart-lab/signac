3addcfc5101349853da44d3260256b6c91d014db
timoast <4591688+timoast@users.noreply.github.com>
2020-01-15 18:30:03 -0500

    Fix bracket

--- R/visualization.R
+++ R/visualization.R
@@ -206,11 +206,11 @@ SingleCoveragePlot <- function(
         } else {
           p <- p + gene.plot + plot_layout(ncol = 1, heights = c(height.tracks, 1))
         }
-    }
     } else {
     if (!is.null(peak.plot)) {
       p <- p + peak.plot + plot_layout(ncol = 1, heights = c(height.tracks, 1))
     }
+   }
   }
   return(p)
 }
