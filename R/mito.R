530fdfa099835a2a1b10fe48b5db2445199f1445
timoast <4591688+timoast@users.noreply.github.com>
2020-03-13 17:36:21 -0400

    Add helper functions

--- R/mito.R
+++ R/mito.R
@@ -95,10 +95,83 @@ SparseMatrixFromBaseCounts <- function(basecounts, cells, dna.base) {
 #' Identify mitochondrial variants present in single cells.
 #'
 #' @param object An Assay object
+#' @param refallele A dataframe containing reference alleles for the
+#' mitochondrial genome.
+#' @param stabilize.variance Stabilize variance
+#' @param low.coverage.threshold Low coverage threshold
 #' @param verbose Display messages
 #'
 #' @return Returns a dataframe
 #' @export
-CallVariants <- function(object, verbose = TRUE, ...) {
+CallVariants <- function(
+  object,
+  refallele,
+  stabilize.variance = TRUE,
+  low.coverage.threshold = 10,
+  verbose = TRUE
+) {
+  # determine key coverage statistics
+  coverages <- ComputeTotalCoverage(object = object, verbose = verbose)
+  ref_allele <- toupper(x = refallele$ref)
+
+  # TODO
   return()
 }
+
+
+####### Not exported
+
+# Compute total mitochondrial coverage
+#
+# Sum the counts for each base and each strand to
+# find the total coverage at each mitochondrial base
+# for each cell
+#
+# Assumes that the matrix is stacked by row, such that
+# the first n rows correspond to the mitochondrial base
+# positions in order, for the first base and first strand,
+# followed by n:2n rows for the next base, etc.
+#
+# @param object A matrix containing coverages for each
+# base and each strand
+# @param verbose Display messages
+# @return Returns a matrix
+ComputeTotalCoverage <- function(object, verbose = TRUE) {
+  if (verbose) {
+    message("Computing total coverage per base")
+  }
+  rowstep <- nrow(x = object) / 8
+  mat.list <- list()
+  for (i in seq_len(length.out = 8)) {
+    mat.list[[i]] <- object[(rowstep * (i-1) + 1):(rowstep*i), ]
+  }
+  coverage <- Reduce(f = `+`, x = mat.list)
+  coverage <- as.matrix(x = coverage)
+  rownames(x = coverage) <- seq_along(along.with = rownames(x = coverage))
+  return(coverage)
+}
+
+# Process mutation for one DNA base
+#
+# @param letter DNA base to process
+# @param verbose Display messages
+ProcessLetter <- function(letter, verbose = TRUE) {
+  # TODO
+  return()
+}
+
+# Extract mutation matrix
+#
+# Subset the mitochondrial count matrix by nucleotide
+# and strand.
+#
+# @param object A matrix containing counts for each
+# mitochondrial base and strand
+# @param letter Which DNA base to use (A, T, C, G)
+# @param strand Which strand to use (fwd, rev)
+# @return Returns a sparse matrix
+GetMutationMatrix <- function(object, letter, strand) {
+  keep.rows <- paste(letter, seq_len(length.out = nrow(x = object) / 8), strand, sep = '-')
+  return(object[keep.rows, ])
+}
+
