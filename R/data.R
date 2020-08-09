3e883b183eecd640c5fe993b4cdffeccc7be3781
timoast <4591688+timoast@users.noreply.github.com>
2020-08-09 12:52:46 -0400

    Fix atac_small docs [ci-skip]

--- R/data.R
+++ R/data.R
@@ -3,13 +3,13 @@
 #' A subsetted version of 10x Genomics 10k human (hg19) PBMC scATAC-seq dataset
 #'
 #' @format A Seurat object with the following assays
-#' @concept data
 #' \describe{
 #'   \item{peaks}{A peak x cell dataset}
 #'   \item{bins}{A 5 kb genome bin x cell dataset}
 #'   \item{RNA}{A gene x cell dataset}
 #' }
 #'
+#' @concept data
 #' @source \url{https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k}
 "atac_small"
 
