5e95f0c65d8294a587b3958241aa9f481707888d
timoast <4591688+timoast@users.noreply.github.com>
2020-10-26 16:37:12 -0400

    Add AlleleFreq test

--- tests/testthat/test-mito.R
+++ tests/testthat/test-mito.R
@@ -25,3 +25,27 @@ test_that("Variant calling is operational", {
   expect_equal(object = length(heteroplasmic_vars), expected = 11)
 
 })
+
+test_that("Allele frequency calculation works", {
+  data.dir <- system.file("extdata", "test_mgatk", package="Signac")
+  mgatk <- ReadMGATK(dir = data.dir)
+  vars.compute <- c("627G>A", "1888G>A", "1888G>C")
+  expected <- new("dgCMatrix",
+                  i = c(1L, 0L, 2L, 1L, 0L, 2L, 1L, 2L, 0L, 0L,1L, 1L, 0L, 2L),
+                  p = c(0L, 1L, 3L, 3L, 4L, 6L, 8L, 9L, 10L, 11L, 12L, 14L),
+                  Dim = c(3L, 11L), Dimnames = list(c("627G>A", "1888G>A", "1888G>C"),
+                                                    c("ACAGGCCGTGGTCGAA-1", "AAACTGCAGAGTCCGA-1", "AAAGATGCAATGGCTT-1",
+                                                      "ACTAGGTAGTGTCGGA-1", "ACCATCCTCTTGTCGC-1", "ACAGAAATCTATCTCA-1",
+                                                      "AATGGCTCAACGTAGG-1", "ACCAAACGTTGTGAGG-1", "AACCGATTCAAGAGAT-1",
+                                                      "ACTAGGTCAATGCCAT-1", "AAAGGGCAGCTACGCC-1")),
+                  x = c(0.0947368421052632,
+                        0.0955631399317406, 0.00273972602739726, 0.108108108108108, 0.0833333333333333,
+                        0.00409836065573771, 0.0256410256410256, 0.00233100233100233,
+                        0.087431693989071, 0.13768115942029, 0.0308788598574822, 0.072,
+                        0.0254237288135593, 0.00689655172413793),
+                  factors = list())
+  alleles <- AlleleFreq(
+    object = mgatk$counts, variants = vars.compute
+  )
+  expect_equal(object = alleles, expected = expected)
+})
