583ffdd60f3ff2bdd35148751b86bbfa320bbd15
timoast <4591688+timoast@users.noreply.github.com>
2022-03-11 11:53:21 -0500

    Clean up

--- R/utilities.R
+++ R/utilities.R
@@ -2189,21 +2189,20 @@ SparseColVar <- function(x) {
   return(SparseRowVar(x = t(x = x)))
 }
 
-#' Replace non-zero entries in a sparse entries with non-zero ranks
-#'
-#' This method creates a rank matrix for a sparse matrix X using the following approach:
-#' 1. Use non-zero enries in a column to calculate the ranks
-#' 2. Add (z-1)/2 to the ranks (only non-zero entries are changed). z is the number of zeros
-#' in the column
-#' Since all the entries are shifted by the same constant (the zeros
-#' are already shifted), the covariance matrix of this shifted matrix is
-#' the same as the rank matrix of the entire matrix (where the zeros would
-#' all also have a rank = (z+1)/2) where z is the number of zeros
-#'
-#' This rank matrix can then be used to calculate pearson correlation
-#' (pearson correlation )
+# Replace non-zero entries in a sparse entries with non-zero ranks
+#
+# This method creates a rank matrix for a sparse matrix X using the following approach:
+# 1. Use non-zero entries in a column to calculate the ranks
+# 2. Add (z-1)/2 to the ranks (only non-zero entries are changed). z is the number of zeros
+# in the column
+# Since all the entries are shifted by the same constant (the zeros
+# are already shifted), the covariance matrix of this shifted matrix is
+# the same as the rank matrix of the entire matrix (where the zeros would
+# all also have a rank = (z+1)/2) where z is the number of zeros
+#
+# This rank matrix can then be used to calculate pearson correlation
 SparsifiedRanks <- function(X){
-  if (class(X)[1] != "dgCMatrix") {
+  if (!inherits(x = X, what = "dgCMatrix")) {
     X <- as(object = X, Class = "dgCMatrix")
   }
   non_zeros_per_col <- diff(x = X@p)
@@ -2211,10 +2210,17 @@ SparsifiedRanks <- function(X){
   offsets <- (n_zeros_per_col - 1) / 2
   x <- X@x
   ## split entries to columns
-  col_lst <- split(x = x, f = rep.int(x = 1:ncol(X), times = non_zeros_per_col))
+  col_lst <- split(
+    x = x,
+    f = rep.int(x = 1:ncol(x = X), times = non_zeros_per_col)
+  )
   ## calculate sparsified ranks and do shifting
-  sparsified_ranks <- unlist(x = lapply(X = seq_along(col_lst),
-                                        FUN = function(i) rank(x = col_lst[[i]]) + offsets[i]))
+  sparsified_ranks <- unlist(
+    x = lapply(
+      X = seq_along(col_lst),
+      FUN = function(i) rank(x = col_lst[[i]]) + offsets[i]
+      )
+    )
   ## Create template rank matrix
   X.ranks <- X
   X.ranks@x <- sparsified_ranks
