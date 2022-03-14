f498b4cd9f9fc867daa74c03c634515209ece73b
timoast <4591688+timoast@users.noreply.github.com>
2022-03-14 11:38:52 -0400

    Fix bug in CoveragePlot when tile=TRUE

--- R/visualization.R
+++ R/visualization.R
@@ -1020,8 +1020,9 @@ SingleCoveragePlot <- function(
   }
   if (tile) {
     # reuse cut matrix
+    # TODO implement for multi assay
     tile.df <- ComputeTile(
-      cutmatrix = cutmat[[1]],
+      cutmatrix = cm.list[[1]],
       groups = obj.groups,
       window = tile.size,
       n = tile.cells,
@@ -1335,7 +1336,8 @@ CoverageTrack <- function(
 #' metadata column in the GRanges object named "color" containing the color to
 #' use for each region.
 #' @param links Display links
-#' @param tile Display per-cell fragment information in sliding windows.
+#' @param tile Display per-cell fragment information in sliding windows. If
+#' plotting multi-assay data, only the first assay is shown in the tile plot.
 #' @param tile.size Size of the sliding window for per-cell fragment tile plot
 #' @param tile.cells Number of cells to display fragment information for in tile
 #' plot.
