02b31ceee460809b8261adcaf611cb9c2d1a99bd
timoast <4591688+timoast@users.noreply.github.com>
2023-04-26 13:38:10 +0800

    Add DensityScatter

--- R/visualization.R
+++ R/visualization.R
@@ -222,6 +222,137 @@ DepthCor <- function(object, assay = NULL, reduction = 'lsi', n = 10, ...) {
   return(p)
 }
 
+# Get density of points in 2 dimensions.
+#
+# Credit to Kamil Slowikowski
+# https://slowkow.com/notes/ggplot2-color-by-density/
+#
+# @param x A numeric vector.
+# @param y A numeric vector.
+# @param n Create a square n by n grid to compute density.
+# @return The density within each square.
+get_density <- function(x, y, ...) {
+  if (!requireNamespace("MASS", quietly = TRUE)) {
+    stop("Please install MASS: install.packages('MASS')")
+  }
+  dens <- kde2d(x = x, y = y, ...)
+  ix <- findInterval(x = x, vec = dens$x)
+  iy <- findInterval(x = y, vec = dens$y)
+  ii <- cbind(ix, iy)
+  return(dens$z[ii])
+}
+
+globalVariables(".data")
+#' Scatterplot colored by point density
+#' 
+#' Create a scatterplot using variables in the object metadata
+#' and color cells by the density of points in the x-y space.
+#'
+#' @param object A Seurat object
+#' @param x Name of metadata variable to plot on x axis
+#' @param y Name of metadata variable to plot on y axis
+#' @param log_x log10 transform x values
+#' @param log_y log10 transform y values
+#' @param quantiles Vector of quantiles to display
+#' for x and y data distribution. Must be integer values
+#' between 0 and 100.
+#' TRUE can be passed as a shorthand way to set
+#' \code{c(5, 10, 90, 95)}. If FALSE or NULL, no quantile
+#' information is displayed
+#' @return Returns a ggplot object
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_viridis_c
+#' theme_bw scale_x_log10 scale_y_log10 geom_vline geom_hline labs
+#' @importFrom rlang .data
+#' @importFrom stats quantile
+DensityScatter <- function(
+    object,
+    x,
+    y,
+    log_x = FALSE,
+    log_y = FALSE,
+    quantiles = NULL
+) {
+    md <- object[[]]
+    log10p <- function(x) {
+        return(log10(x = x + 1))
+    }
+    null_fn <- function(x) {
+        return(x)
+    }
+    logfnx <- ifelse(test = log_x, yes = log10p, no = null_fn)
+    logfny <- ifelse(test = log_y, yes = log10p, no = null_fn)
+    # logfny <- null_fn
+    md$Density <- get_density(
+        x = logfnx(md[[x]]),
+        y = logfny(md[[y]]),
+        n = 1000,
+        h = c(1,1)
+    )
+    md <- md[order(md$Density), ]
+    # quantiles
+    use_quantile <- FALSE
+    if (!is.null(x = quantiles)) {
+        use_quantile <- TRUE
+        # set default if TRUE passed
+        if (is.logical(x = quantiles)) {
+            if (quantiles) {
+                quantiles <- c(5, 10, 90, 95)
+            } else {
+                use_quantile <- FALSE
+            }
+        }
+        # make sure integers between 0 and 100
+        if (!(all(quantiles >= 0) & all(quantiles <= 100))) {
+            warning("Quantile values must be between 0 and 100",
+                    immediate. = TRUE)
+            use_quantile <- FALSE
+        }
+        if (any(sapply(X = quantiles, FUN = function(x) x %% 1 != 0))) {
+            warning("Quantile values must be integers",
+                    immediate. = TRUE)
+            use_quantile <- FALSE
+        }
+    }
+    if (use_quantile) {
+        # convert to string
+        quantiles <- paste0(quantiles, '%')
+
+        # quantiles for x and y
+        x_quant <- quantile(x = md[[x]], probs = seq(0, 1, 0.01))
+        y_quant <- quantile(x = md[[y]], probs = seq(0, 1, 0.01))
+        xlines <- x_quant[quantiles]
+        ylines <- y_quant[quantiles]
+
+        # round
+        xlines <- round(x = xlines, digits = 2)
+        ylines <- round(x = ylines, digits = 2)
+    }        
+    p <- ggplot(
+           data = md,
+           mapping = aes(x = .data[[x]], y = .data[[y]], color = .data[["Density"]])
+         ) +
+           geom_point(size = 1) +
+           scale_color_viridis_c(option = "B") +
+           theme_bw()
+    if (log_x) {
+        p <- p + scale_x_log10()
+    }
+    if (log_y) {
+        p <- p + scale_y_log10()
+    }
+    if (use_quantile) {
+        p <- p +
+          geom_vline(xintercept = unname(obj = xlines), color = "red") +
+          geom_hline(yintercept = unname(obj = ylines), color = "red") +
+          labs(title = "Quantiles",
+               subtitle = paste0(x, ": ", paste0(names(x = xlines), ":",
+                                                 xlines, collapse = " "), "\n",
+                                 y, ": ", paste0(names(x = ylines), ":",
+                                                 ylines, collapse = " ")))
+    }
+    return(p)
+}
+
 globalVariables(
   names = c("feature", "group", "mn", "norm.value"),
   package = "Signac"
