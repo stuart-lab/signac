0d5eee5b3bd6fb2f5118494de14ed5304c398eb8
timoast <4591688+timoast@users.noreply.github.com>
2023-06-23 17:37:23 +0800

    Reformat

--- R/utilities.R
+++ R/utilities.R
@@ -2297,38 +2297,31 @@ ExportGroupBW  <- function(
     chromosome = NULL,
     outdir = NULL,
     verbose=TRUE
-){
-  
-  #Check if temporary directory exist
+) {
+  # Check if temporary directory exist
   if (!dir.exists(outdir)){
     dir.create(outdir)
   }
-  
   if (!requireNamespace("rtracklayer", quietly = TRUE)) { 
     message("Please install rtracklayer. http://www.bioconductor.org/packages/rtracklayer/") 
     return(NULL) 
   }
-  
-  assay <- assay %||% DefaultAssay(object = object)
+  assay <- SetIfNull(x = assay, y = DefaultAssay(object = object))
   DefaultAssay(object = object) <- assay
-  
-  group.by <- group.by %||% 'ident'
+  group.by <- SetIfNull(x = group.by, y = 'ident')
   Idents(object = object) <- group.by
-  
-  idents <- idents %||% levels(object)
-  
-  GroupsNames <- names(table(object[[group.by]])[table(object[[group.by]]) > minCells])
+  idents <- SetIfNull(x = idents, y = levels(x = object))
+  GroupsNames <- names(x = table(object[[group.by]])[table(object[[group.by]]) > minCells])
   GroupsNames <- GroupsNames[GroupsNames %in% idents]
-  
-  #Check if output files already exist
-  lapply(GroupsNames, function(x){
-    if (file.exists(paste0(outdir, .Platform$file.sep, x, ".bed"))){
+  # Check if output files already exist
+  lapply(X = GroupsNames, FUN = function(x) {
+    fn <- paste0(outdir, .Platform$file.sep, x, ".bed")
+    if (file.exists(fn)) {
       message(sprintf("The group \"%s\" is already present in the destination folder and will be overwritten !",x))
-      file.remove(paste0(outdir, .Platform$file.sep, x, ".bed"))
+      file.remove(fn)
     }
   })      
-  
-  #Splitting fragments file for each idents in group.by
+  # Splitting fragments file for each idents in group.by
   SplitFragments(
     object = object,
     assay = assay,
@@ -2340,48 +2333,57 @@ ExportGroupBW  <- function(
     buffer_length = 256L,
     verbose = verbose
   )
-  
-  #Column to normalized by
-  if(!is.null(normMethod)){
-    if (tolower(normMethod) %in% c('rc', 'ncells', 'none')){
+  # Column to normalized by
+  if(!is.null(x = normMethod)) {
+    if (tolower(x = normMethod) %in% c('rc', 'ncells', 'none')){
       normBy <- normMethod
     } else{
-      normBy <- object[[normMethod, drop=FALSE]]
+      normBy <- object[[normMethod, drop = FALSE]]
     }
   }
-  
-  #Get chromosome information
-  if(!is.null(chromosome)){
+  # Get chromosome information
+  if(!is.null(x = chromosome)){
     seqlevels(object) <- chromosome
   }
-  
-  availableChr <- names(seqlengths(object))
+  availableChr <- names(x = seqlengths(object))
   chromLengths <- seqlengths(object)
-  chromSizes <- GRanges(seqnames = availableChr, IRanges(start = rep(1, length(availableChr)), end = as.numeric(chromLengths)))
+  chromSizes <- GRanges(
+    seqnames = availableChr,
+    ranges = IRanges(
+      start = rep(1, length(x = availableChr)),
+      end = as.numeric(x = chromLengths)
+      )
+    )
   
   if (verbose) {
-    message("Creation of tiles")
+    message("Creating tiles")
   }
-  
-  #Create tiles for each chromosome, from GenomicRanges
-  tiles <- unlist(slidingWindows(chromSizes, width = tileSize, step = tileSize))
-  
+  # Create tiles for each chromosome, from GenomicRanges
+  tiles <- unlist(
+    x = slidingWindows(x = chromSizes, width = tileSize, step = tileSize)
+  )
   if (verbose) {
-    message("Bigwig files generation at ", outdir)
+    message("Creating bigwig files at ", outdir)
   }
-  
-  #Run the creation of bigwig for each cellgroups
-  
+  # Run the creation of bigwig for each cellgroups
   if (nbrOfWorkers() > 1) { 
     mylapply <- future_lapply 
   } else { 
     mylapply <- ifelse(test = verbose, yes = pblapply, no = lapply) 
   }
-  
-  covFiles <- mylapply(GroupsNames, CreateBWGroup, availableChr, chromLengths, tiles, normBy, tileSize, normMethod, cutoff, outdir)
-  
-  covFiles
-  
+  covFiles <- mylapply(
+    x = GroupsNames,
+    FUN = CreateBWGroup,
+    availableChr,
+    chromLengths,
+    tiles,
+    normBy,
+    tileSize,
+    normMethod,
+    cutoff,
+    outdir
+  )
+  return(covFiles)
 }
 
 # Helper function for ExportGroupBW
@@ -2407,79 +2409,86 @@ ExportGroupBW  <- function(
 #' @importFrom BiocGenerics which
 #' @importFrom S4Vectors match
 #' @importFrom Matrix sparseMatrix rowSums
-CreateBWGroup <- function(groupNamei, availableChr, chromLengths, tiles, normBy, tileSize, normMethod, cutoff, outdir){
+CreateBWGroup <- function(
+    groupNamei,
+    availableChr,
+    chromLengths,
+    tiles,
+    normBy,
+    tileSize,
+    normMethod,
+    cutoff,
+    outdir
+) {
+  if (!requireNamespace("rtracklayer", quietly = TRUE)) { 
+    message("Please install rtracklayer. http://www.bioconductor.org/packages/rtracklayer/") 
+    return(NULL) 
+  }
+  normMethod <- tolower(x = normMethod)
+  # Read the fragments file associated to the group
+  fragi <- rtracklayer::import(
+    paste0(outdir, .Platform$file.sep, groupNamei, ".bed"), format = "bed"
+  )
+  cellGroupi <- unique(x = fragi$name)
+  # Open the writing bigwig file
+  covFile <- file.path(
+    outdir,
+    paste0(groupNamei, "-TileSize-",tileSize,"-normMethod-",normMethod,".bw")
+  )
   
-  #Read the fragments file associated to the group
-  fragi <- rtracklayer::import(paste0(outdir, .Platform$file.sep, groupNamei, ".bed"),format = "bed")
-  
-  cellGroupi <- unique(fragi$name)
-  
-  #Open the writting bigwig file
-  covFile <- file.path(outdir, paste0(groupNamei, "-TileSize-",tileSize,"-normMethod-",normMethod,".bw"))
-  
-  covList <- lapply(seq_along(availableChr), function(k){
-    
+  covList <- lapply(X = seq_along(availableChr), FUN = function(k) {
     fragik <- fragi[seqnames(fragi) == availableChr[k],]
     tilesk <- tiles[BiocGenerics::which(S4Vectors::match(seqnames(tiles), availableChr[k], nomatch = 0) > 0)]
-    
-    if(length(fragik) == 0){
+    if (length(x = fragik) == 0) {
       tilesk$reads <- 0
-      #If fragments
-    }else{
-      
-      #N Tiles
+      # If fragments
+    } else {
+      # N Tiles
       nTiles <- chromLengths[availableChr[k]] / tileSize
-      #Add one tile if there is extra bases
+      # Add one tile if there is extra bases
       if (nTiles%%1 != 0) {
-        nTiles <- trunc(nTiles) + 1
+        nTiles <- trunc(x = nTiles) + 1
       }
-      
-      #Create Sparse Matrix
+      # Create Sparse Matrix
       matchID <- S4Vectors::match(mcols(fragik)$name, cellGroupi)
       
-      #For each tiles of this chromosome, create start tile and end tile row, set the associated counts matching with the fragments
-      mat <- Matrix::sparseMatrix(
-        i = c(trunc(start(fragik) / tileSize), trunc(end(fragik) / tileSize)) + 1,
-        j = as.vector(c(matchID, matchID)),
-        x = rep(1, 2*length(fragik)),
-        dims = c(nTiles, length(cellGroupi)))
+      # For each tiles of this chromosome, create start tile and end tile row,
+      # set the associated counts matching with the fragments
+      mat <- sparseMatrix(
+        i = c(trunc(x = start(x = fragik) / tileSize),
+              trunc(x = end(x = fragik) / tileSize)) + 1,
+        j = as.vector(x = c(matchID, matchID)),
+        x = rep(1, 2*length(x = fragik)),
+        dims = c(nTiles, length(x = cellGroupi))
+      )
       
-      #Max count for a cells in a tile is set to cutoff
-      if(!is.null(cutoff)){
+      # Max count for a cells in a tile is set to cutoff
+      if (!is.null(x = cutoff)){
         mat@x[mat@x > cutoff] <- cutoff
       }
-      
-      #Sums the cells
-      mat <- Matrix::rowSums(mat)
-      
+      # Sums the cells
+      mat <- rowSums(x = mat)
       tilesk$reads <- mat
-      
-      #Normalization
-      if(!is.null(normMethod)){
-        if(tolower(normMethod) == "rc"){
+      # Normalization
+      if (!is.null(x = normMethod)) {
+        if (normMethod == "rc") {
           tilesk$reads <- tilesk$reads * 10^4 / length(fragi$name)
-        }else if(tolower(normMethod) == "ncells"){
+        } else if (normMethod == "ncells") {
           tilesk$reads <- tilesk$reads / length(cellGroupi)
-        }else if(tolower(normMethod) == "none"){
-        }else{
-          if(!is.null(normBy)){
+        } else if (normMethod == "none") {
+        } else {
+          if (!is.null(x = normBy)){
             tilesk$reads <- tilesk$reads * 10^4 / sum(normBy[cellGroupi, 1])
           }
         }
       }
     }
-    
     tilesk <- coverage(tilesk, weight = tilesk$reads)[[availableChr[k]]]
-    
     tilesk
-    
   })
   
   names(covList) <- availableChr
-  
-  covList <- as(covList, "RleList")
-  
+  covList <- as(object = covList, Class = "RleList")
   rtracklayer::export.bw(object = covList, con = covFile)
-  
   return(covFile)
 }
