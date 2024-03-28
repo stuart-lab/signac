311ef3af430b99ef315eb51d107719b3e7868886
timoast <4591688+timoast@users.noreply.github.com>
2024-03-28 13:31:33 +0800

    Update docs for GetLinkedPeaks/Genes; #1654

--- R/links.R
+++ R/links.R
@@ -3,8 +3,15 @@
 NULL
 
 #' Get peaks linked to genes
-#'
-#' Find peaks linked to a given set of genes
+#' 
+#' Retrieve peak-gene links for a given set of genes. Links must be first
+#' obtained by running the \code{LinkPeaks} function.
+#' 
+#' This function is designed to obtain the stored results from running the
+#' \code{LinkPeaks} function. Alternatively, custom peak-gene linkage methods
+#' can be used as long as they store the gene name, peak name, and a peak-gene
+#' score information as metadata columns named "gene," "peak," and "score"
+#' respectively.
 #'
 #' @param object A Seurat object
 #' @param features A list of genes to find linked peaks for
@@ -33,8 +40,15 @@ GetLinkedPeaks <- function(
 }
 
 #' Get genes linked to peaks
-#'
-#' Find genes linked to a given set of peaks
+#' 
+#' Retrieve peak-gene links for a given set of genes. Links must be first
+#' obtained by running the \code{LinkPeaks} function.
+#' 
+#' This function is designed to obtain the stored results from running the
+#' \code{LinkPeaks} function. Alternatively, custom peak-gene linkage methods
+#' can be used as long as they store the gene name, peak name, and a peak-gene
+#' score information as metadata columns named "gene," "peak," and "score"
+#' respectively.
 #'
 #' @param object A Seurat object
 #' @param features A list of peaks to find linked genes for
@@ -290,6 +304,10 @@ LinkPeaks <- function(
   peak.data <- GetAssayData(
     object = object, assay = peak.assay, slot = peak.slot
   )
+  # # depends on SeuratObject v5
+  # if (!(expression.slot %in% Layers(object = object))) {
+  #   stop("Requested expression layer not found")
+  # }
   expression.data <- GetAssayData(
     object = object, assay = expression.assay, slot = expression.slot
   )
