4dc3441faf9c8fcf597c17b92d8759964b7bf5f0
timoast <4591688+timoast@users.noreply.github.com>
2025-01-13 14:00:44 +0800

    Add ReadCounts

--- R/utilities.R
+++ R/utilities.R
@@ -1161,6 +1161,28 @@ UnifyPeaks <- function(object.list, mode = "reduce") {
   }
 }
 
+#' Read counts
+#' 
+#' Read count matrix from mtx file
+#' 
+#' @param dir Path to directory containing the matrix.mtx,
+#' barcodes.tsv, and features.tsv files
+#' @concept utilities
+#' @export
+#' @return Returns a sparse matrix
+#' @importFrom Matrix readMM
+ReadCounts <- function(dir) {
+  counts <- readMM(paste0(dir, "matrix.mtx.gz"))
+  if (file.exists(paste0(dir, "barcodes.tsv.gz"))) {
+    colnames(counts) <- readLines(paste0(dir, "barcodes.tsv.gz"))
+  } else {
+    colnames(counts) <- readLines(paste0(dir, "barcodes.tsv"))
+  }
+  rownames(counts) <- readLines(paste0(dir, "features.tsv.gz"))
+  counts <- as(counts, "CsparseMatrix")
+  return(counts)
+}
+
 #' Subset matrix rows and columns
 #'
 #' Subset the rows and columns of a matrix by removing
