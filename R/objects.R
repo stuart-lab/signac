dcb0f4e0a8496e5a9c3241e64b01f43fe54b82aa
timoast <4591688+timoast@users.noreply.github.com>
2025-08-22 13:30:51 +0800

    Update object classes

--- R/objects.R
+++ R/objects.R
@@ -15,13 +15,18 @@ setClassUnion(name = "AnyMatrix", c("matrix", "dgCMatrix"))
 #' The Fragment class is designed to hold information needed for working with
 #' fragment files.
 #'
-#' @slot path Path to the fragment file on disk.
+#' @slot file.path Path to the fragment file on disk.
 #' See \url{https://support.10xgenomics.com/single-cell-atac/software/pipelines/latest/output/fragments}
+#' @slot file.index Path to the fragment file index on disk.
 #' @slot hash A vector of two md5sums: first element is the md5sum of the
 #' fragment file, the second element is the md5sum of the index.
 #' @slot cells A named vector of cells where each element is the cell barcode
 #' as it appears in the fragment file, and the name of each element is the
-#' corresponding cell barcode as stored in the ChromatinAssay object.
+#' corresponding cell barcode as stored in the ChromatinAssay5 object.
+#' @slot seqnames A named vector of sequence names (eg, chromosome name) where
+#' each element is the sequence name as it appears in the fragment file, and the
+#' name of each element is the corresponding sequence name as stored in the 
+#' ChromatinAssay5 object.
 #'
 #' @name Fragment-class
 #' @rdname Fragment-class
@@ -30,9 +35,11 @@ setClassUnion(name = "AnyMatrix", c("matrix", "dgCMatrix"))
 Fragment <- setClass(
   Class = "Fragment",
   slots = list(
-    path = "character",
+    file.path = "character",
+    file.index = "character",
     hash = "character",
-    cells = "ANY"
+    cells = "ANY",
+    seqnames = "ANY"
   )
 )
 
@@ -68,14 +75,12 @@ Motif <- setClass(
   )
 )
 
-#' The ChromatinAssay class
+#' The ChromatinAssay5 class
 #'
-#' The ChromatinAssay object is an extended \code{\link[SeuratObject]{Assay}}
-#' for the storage and analysis of single-cell chromatin data.
+#' The ChromatinAssay5 object is an extended \code{\link[SeuratObject]{Assay5}}
+#' for the storage and analysis of single-cell chromatin data. This class does
+#' not require that the stored features are genomic ranges.
 #'
-#' @slot ranges A \code{\link[GenomicRanges]{GRanges}} object describing the
-#' genomic location of features in the object
-#' @slot motifs A \code{\link{Motif}} object
 #' @slot fragments A list of \code{\link{Fragment}} objects.
 #' @slot seqinfo A \code{\link[GenomeInfoDb]{Seqinfo}} object containing basic
 #' information about the genome sequence used.
@@ -93,35 +98,56 @@ Motif <- setClass(
 #' (frequency of Tn5 integration at different kmers)
 #' @slot positionEnrichment A named list of matrices containing positional
 #' enrichment scores for Tn5 integration (for example, enrichment at the TSS)
+#'
+#' @name ChromatinAssay5-class
+#' @rdname ChromatinAssay5-class
+#' @importClassesFrom SeuratObject Assay5
+#' @exportClass ChromatinAssay5
+#' @concept assay
+ChromatinAssay5 <- setClass(
+  Class = "ChromatinAssay5",
+  contains = "Assay5",
+  slots = list(
+    "fragments" = "list",
+    "seqinfo" = "ANY",
+    "annotation" = "ANY",
+    "bias" = "ANY",
+    "positionEnrichment" = "list"
+  )
+)
+
+#' The GRangesAssay class
+#'
+#' The GRangesAssay object is an extended \code{\link{ChromatinAssay5}}
+#' for the storage and analysis of single-cell chromatin data with associated
+#' genomic ranges.
+#'
+#' @slot ranges A \code{\link[GenomicRanges]{GRanges}} object describing the
+#' genomic location of features in the object
+#' @slot motifs A \code{\link{Motif}} object
 #' @slot links A \code{\link[GenomicRanges]{GRanges}} object describing linked
 #' genomic positions, such as co-accessible sites or enhancer-gene regulatory
 #' relationships. This should be a \code{GRanges} object, where the start and
 #' end coordinates are the two linked genomic positions, and must contain a
 #' "score" metadata column.
 #'
-#' @name ChromatinAssay-class
-#' @rdname ChromatinAssay-class
-#' @importClassesFrom SeuratObject Assay
-#' @exportClass ChromatinAssay
+#' @name GRangesAssay-class
+#' @rdname GRangesAssay-class
+#' @exportClass GRangesAssay
 #' @concept assay
-ChromatinAssay <- setClass(
-  Class = "ChromatinAssay",
-  contains = "Assay",
+GRangesAssay <- setClass(
+  Class = "GRangesAssay",
+  contains = "ChromatinAssay5",
   slots = list(
     "ranges" = "GRanges",
-    "motifs" = "ANY",
-    "fragments" = "list",
-    "seqinfo" = "ANY",
-    "annotation" = "ANY",
-    "bias" = "ANY",
-    "positionEnrichment" = "list",
+    "motifs" = "Motif",
     "links" = "GRanges"
   )
 )
 
-#' Create ChromatinAssay object
+#' Create ChromatinAssay5 object
 #'
-#' Create a \code{\link{ChromatinAssay}} object from a count matrix or
+#' Create a \code{\link{ChromatinAssay5}} object from a count matrix or
 #' normalized data matrix. The expected format of the input matrix is features x
 #' cells. A set of genomic ranges must be supplied along with the matrix, with
 #' the length of the ranges equal to the number of rows in the matrix. If a set
