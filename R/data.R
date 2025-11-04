bd3eac6754f2b3c0d36c56849b47b739ead7705f
timoast <4591688+timoast@users.noreply.github.com>
2025-11-04 12:30:31 +0800

    Update atac_small for v2

--- R/data.R
+++ R/data.R
@@ -1,6 +1,21 @@
 #' A small example scATAC-seq dataset
 #'
-#' A subsetted version of 10x Genomics 10k human (hg19) PBMC scATAC-seq dataset
+#' A subsetted version of 10x Genomics 10k human (hg38) PBMC scATAC-seq dataset
+#'
+#' @format A Seurat object with the following assays
+#' \describe{
+#'   \item{peaks}{A peak x cell dataset}
+#'   \item{RNA}{A gene x cell dataset}
+#' }
+#'
+#' @concept data
+#' @source \url{https://www.10xgenomics.com/datasets/10k-human-pbmcs-atac-v2-chromium-controller-2-standard}
+"atac_small"
+
+#' A small example scATAC-seq dataset (old)
+#'
+#' A subsetted version of 10x Genomics 10k human (hg19) PBMC scATAC-seq dataset.
+#' This object was included in Signac v1 as the small test data object.
 #'
 #' @format A Seurat object with the following assays
 #' \describe{
@@ -11,4 +26,4 @@
 #'
 #' @concept data
 #' @source \url{https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_v1_pbmc_10k}
-"atac_small"
+"atac_small_old"
---
+++ data-raw/atac_small.R
@@ -0,0 +1,41 @@
+## code to prepare atac_small
+
+library(Signac)
+library(Seurat)
+library(AnnotationHub)
+setwd("vignette_data/pbmc_vignette/")
+set.seed(1234)
+
+ah <- AnnotationHub()
+ensdb_v98 <- ah[["AH75011"]]
+annotations <- GetGRangesFromEnsDb(ensdb = ensdb_v98)
+seqlevels(annotations) <- paste0('chr', seqlevels(annotations))
+genome(annotations) <- "hg38"
+
+counts <- Read10X_h5(filename = "10k_pbmc_ATACv2_nextgem_Chromium_Controller_filtered_peak_bc_matrix.h5")
+
+gr_assay <- CreateGRangesAssay(
+  counts = counts[1:100, 1:100],
+  sep = c(":", "-"),
+  fragments = "10k_pbmc_ATACv2_nextgem_Chromium_Controller_fragments.tsv.gz"
+)
+
+pbmc <- CreateSeuratObject(counts = gr_assay, assay = "peaks")
+
+Annotation(pbmc) <- annotations
+ga <- GeneActivity(pbmc)
+
+ga_subset <- ga[1:50, ]
+
+pbmc[['RNA']] <- CreateAssayObject(counts = ga_subset)
+annotation_subset <- annotations[annotations$gene_name == "CD8A"]
+Annotation(pbmc) <- annotation_subset
+
+pbmc <- RunTFIDF(pbmc)
+pbmc <- RunSVD(pbmc, features = rownames(pbmc))
+pbmc <- RunUMAP(pbmc, reduction = 'lsi', dims = 1:10)
+pbmc$cluster <- sample(c(1, 2), size = ncol(pbmc), replace = TRUE)
+
+Fragments(pbmc) <- NULL
+atac_small <- pbmc
+usethis::use_data(atac_small, overwrite = TRUE)
