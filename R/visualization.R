88cefd4816a3bb8236c1a7b746a2fb31214cd896
timoast <4591688+timoast@users.noreply.github.com>
2026-01-19 17:35:51 +0800

    Update docs; reorganize

--- R/visualization.R
+++ R/visualization.R
@@ -260,6 +260,233 @@ get_density <- function(x, y, n_sub = 50000, ...) {
   return(out)
 }
 
+#' Create GWAS locus zoom track
+#' 
+#' This function will plot the p-values associated with variants in a given
+#' region of the genome (genome position: x-axis; -log10(p): y-axis).
+#' 
+#' If an LD file is provided using the \code{ld.file} parameter, or a column
+#' named \code{r2} is present in the input GWAS data, the variants will be
+#' colored according to their LD value.
+#' 
+#' If a fine mapping file is provided using the \code{credset.file} parameter,
+#' or a column named \code{in_credset} is present in the input GWAS data,
+#' variants in the credible set will be denoted by shape. If LD information is
+#' not provided, credible variants will also be denoted by color.
+#'
+#' @param region Genomic region (GRanges or string like "chr10-112900000-113100000")
+#' @param gwas Path to GWAS summary statistics file, or a dataframe
+#' containing the gwas data in the GWAS-SSF format
+#' @param ld.file Path to LD file. Optional.
+#' @param ld.lead.snp Lead SNP for LD (required if ld.file provided)
+#' @param credset.file Path to credible set file. Optional.
+#' @param credset.threshold PIP threshold for credible sets (default: 0.01)
+#' @param p.threshold Genome-wide significance threshold (default: 5e-8)
+#' @param ymax Maximum y-axis value (default: auto)
+#' @param point.size Point size (default: 1)
+#' @param point.color Point color when no LD (default: "steelblue")
+#' @param show.axis Show x-axis (default: TRUE)
+#' @return ggplot2 object
+#'
+#' @importFrom ggplot2 ggplot geom_point aes_string geom_hline scale_y_continuous
+#' theme_classic labs theme element_blank element_line element_text
+#' scale_shape_manual scale_size_manual scale_color_manual
+#' @importFrom Seqinfo seqnames
+#' @importFrom GenomicRanges start end
+#' @export
+#' @concept visualization
+GWASTrack <- function(
+    gwas,
+    region,
+    ld.file = NULL,
+    ld.lead.snp = NULL,
+    credset.file = NULL,
+    credset.threshold = 0.01,
+    p.threshold = 5e-8,
+    ymax = NULL,
+    point.size = 1,
+    point.color = "steelblue",
+    show.axis = TRUE
+) {
+  
+  # Load GWAS data
+  if (is.character(x = gwas)) {
+    gwas <- LoadGWAS(gwas.file = gwas)
+  } 
+  
+  if (!inherits(x = region, what = "GRanges")) {
+    region <- StringToGRanges(regions = region)
+  }
+  
+  # subset to region
+  chromosome <- as.character(x = seqnames(x = region))
+  gwas <- gwas[
+    gwas[['chromosome']] == chromosome &
+      gwas[['base_pair_location']] >= start(x = region) &
+      gwas[['base_pair_location']] <= end(x = region),
+  ]
+  gwas <- gwas[!is.na(x = gwas[['p_value']]), ]
+  
+  if (nrow(x = gwas) == 0) {
+    stop("No GWAS data found in region")
+  }
+  gwas[['log10p']] <- -log10(x = gwas[['p_value']])
+  
+  # Validate LD parameters
+  if (!is.null(x = ld.file) && is.null(x = ld.lead.snp)) {
+    stop("ld.lead.snp required when ld.file provided")
+  }
+  
+  # LocusZoom colors
+  ld_colors <- c(
+    "r2_0-0.2" = "#0000CD",
+    "r2_0.2-0.4" = "#00CED1",
+    "r2_0.4-0.6" = "#32CD32",
+    "r2_0.6-0.8" = "#FFA500",
+    "r2_0.8-1.0" = "#FF0000"
+  )
+  
+  # Merge LD data
+  if (!is.null(x = ld.file)) {
+    ld_data <- LoadLDData(ld.file = ld.file)
+    gwas <- merge(
+      x = gwas,
+      y = ld_data,
+      by = c("chromosome", "base_pair_location"),
+      all.x = TRUE
+    )
+    gwas[['r2']] <- as.numeric(x = gwas[['r2']])
+  }
+  
+  if ('r2' %in% colnames(x = gwas)) {
+    gwas[['ld_category']] <- cut(
+      gwas[['r2']],
+      breaks = c(-Inf, 0.2, 0.4, 0.6, 0.8, Inf),
+      labels = c("r2_0-0.2", "r2_0.2-0.4", "r2_0.4-0.6", "r2_0.6-0.8", "r2_0.8-1.0"),
+      include.lowest = TRUE
+    )
+  }
+  
+  # Merge credible set data
+  if (!is.null(x = credset.file)) {
+    credset_data <- LoadCredibleSets(
+      credset.file = credset.file, credset.threshold = credset.threshold
+    )
+    gwas <- merge(
+      x = gwas,
+      y = credset_data,
+      by = c("chromosome", "base_pair_location"),
+      all.x = TRUE
+    )
+    gwas[['in_credset']] <- !is.na(x = gwas[['pip']])
+  }
+  
+  # Y-axis limit
+  if (is.null(x = ymax)) {
+    ymax <- max(gwas[['log10p']], na.rm = TRUE) * 1.1
+  }
+  
+  # Build plot
+  if ("in_credset" %in% colnames(x = gwas)) {
+    if ("ld_category" %in% colnames(x = gwas)) {
+      # LD + credible sets
+      p <- ggplot(data = gwas, mapping = aes_string(
+        x = 'base_pair_location',
+        y = 'log10p',
+        color = 'ld_category'
+      )) +
+        geom_point(
+          aes_string(shape = 'in_credset', size = 'in_credset'), alpha = 0.6
+        ) +
+        scale_shape_manual(
+          values = c("FALSE" = 16, "TRUE" = 18),
+          guide = "none"
+        ) +
+        scale_size_manual(
+          values = c("FALSE" = point.size, "TRUE" = point.size * 2),
+          guide = "none"
+        ) +
+        scale_color_manual(
+          values = ld_colors,
+          name = expression(LD~(r^2)),
+          na.value = "grey50"
+        )
+    } else {
+      # Credible sets only
+      p <- ggplot(data = gwas, mapping = aes_string(
+        x = 'base_pair_location',
+        y = 'log10p')) +
+        geom_point(
+          mapping = aes_string(
+            shape = 'in_credset',
+            size = 'in_credset',
+            color = 'in_credset'),
+          alpha = 0.6
+        ) +
+        scale_shape_manual(
+          values = c("FALSE" = 16, "TRUE" = 18),
+          guide = "none"
+        ) +
+        scale_size_manual(
+          values = c("FALSE" = point.size, "TRUE" = point.size * 2),
+          guide = "none"
+        ) +
+        scale_color_manual(
+          values = c("FALSE" = point.color, "TRUE" = "#FF0000"),
+          name = "Credible set",
+          labels = c("FALSE" = "No", "TRUE" = "Yes")
+        )
+    }
+    
+  } else if ("ld_category" %in% colnames(x = gwas)) {
+    # LD only
+    p <- ggplot(
+      data = gwas, mapping = aes_string(
+        x = 'base_pair_location',
+        y = 'log10p',
+        color = 'ld_category'
+      )
+    ) +
+      geom_point(size = point.size, alpha = 0.6) +
+      scale_color_manual(
+        values = ld_colors,
+        name = expression(LD~(r^2)),
+        na.value = "grey50"
+      )
+  } else {
+    # Basic plot
+    p <- ggplot(
+      data = gwas, mapping = aes_string(
+        x = 'base_pair_location',
+        y = 'log10p'
+      )
+    ) +
+      geom_point(color = point.color, size = point.size, alpha = 0.6)
+  }
+  
+  # Common elements
+  p <- p +
+    geom_hline(
+      yintercept = -log10(x = p.threshold),
+      linetype = "dashed",
+      color = "red",
+      alpha = 0.5
+    ) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, ymax)) +
+    theme_classic() +
+    labs(x = paste0(chromosome, " position (bp)"),
+         y = expression(-log[10](italic(P)))) +
+    theme(
+      axis.title.x = if (show.axis) element_text() else element_blank(),
+      axis.text.x = if (show.axis) element_text() else element_blank(),
+      axis.line.x = if (show.axis) element_line() else element_blank(),
+      axis.ticks.x = if (show.axis) element_line() else element_blank()
+    ) +
+    xlim(c(start(x = region), end(x = region)))
+  
+  return(p)
+}
+
 globalVariables(".data")
 #' Scatterplot colored by point density
 #' 
