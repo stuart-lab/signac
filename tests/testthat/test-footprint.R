8306c2e07e13df66c00c54ec9460a636d2568aed
TingWeee <tingwei.ltw@gmail.com>
2026-02-12 16:59:34 +0800

    test-footprint.R

---
+++ tests/testthat/test-footprint.R
@@ -0,0 +1,24 @@
+suppressWarnings(RNGversion(vstr = "3.5.3"))
+
+fpath_headered <- system.file("extdata", "fragments_header.tsv.gz", package="Signac")
+cells <- colnames(x = atac_small)
+frags <- CreateFragmentObject(path = fpath_headered, cells = cells, verbose = FALSE, tolerance = 0.5)
+Fragments(atac_small) <- NULL
+Fragments(atac_small, layer="fragments") <- frags
+pwm <- readRDS("../testdata/pwm_2motifs.rds")
+genome.fasta <- system.file("extdata", "chr1_start.fa", package = "Signac")
+genome <- Rsamtools::FaFile(genome.fasta)
+
+test_that("Footprint works", {
+  skip_if_not_installed("motifmatchr")
+  expect_warning(test_atac <- AddMotifs(atac_small, genome, pwm, verbose=FALSE))
+  test_atac <- Footprint(object=test_atac, 
+                         motif.name = names(pwm),
+                         compute.expected = FALSE, 
+                         genome=genome)
+  test_df <- GetFootprintData(test_atac, features = c("MA0031.1"))
+  expect_equal(sum(test_df$count), 1013)
+  
+  test_width <- length(pwm[[1]]) + RegionAggr(test_atac)[[1]]@upstream + RegionAggr(test_atac)[[1]]@downstream
+  expect_equal(dim(RegionAggr(test_atac) [[1]]@matrix), c(100, test_width))
+})
